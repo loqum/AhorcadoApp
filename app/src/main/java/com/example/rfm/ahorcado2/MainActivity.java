2eae3b3aaf2e0ee61aacfc3964eb80c85ea138ee
loqum <ruben693@gmail.com>
2019-02-28 15:48:34 +0100

    Refactorizado código, añadido toast y contador de errores.

--- app/src/main/java/com/example/rfm/ahorcado2/MainActivity.java
+++ app/src/main/java/com/example/rfm/ahorcado2/MainActivity.java
@@ -6,37 +6,65 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView labelPalabraSecreta;
+    private TextView labelContador;
+    private EditText inputRespuesta;
+    private String palabraSecreta;
+    private String interrogante;
+    private int contador = 0;
+    private boolean isCorrect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonIntentar = (Button) findViewById(R.id.buttonJugar);
-        final TextView labelPalabraSecreta = (TextView) findViewById(R.id.labelPalabraSecreta);
-        final EditText inputRespuesta = (EditText) findViewById(R.id.inputRespuesta);
+        Button buttonIntentar = findViewById(R.id.buttonJugar);
+        labelPalabraSecreta = findViewById(R.id.labelPalabraSecreta);
+        labelContador = findViewById(R.id.labelContador);
+        inputRespuesta = findViewById(R.id.inputRespuesta);
 
-        final String palabraSecreta = "Arbol";
-        final String interrogante = new String(new char[palabraSecreta.length()]).replace("\0", "?");
+        palabraSecreta = "asamblea";
+        interrogante = new String(new char[palabraSecreta.length()]).replace("\0", "?");
 
         final String[] letra = palabraSecreta.split("");
 
-        labelPalabraSecreta.setText(palabraSecreta.replaceAll("([A-Z,a-z])", "?"));
+        labelPalabraSecreta.setText(palabraSecreta.replaceAll("([A-Za-z])", "?"));
 
         buttonIntentar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for (int i = 1; i < letra.length; i ++) {
-                    if (letra[i].equalsIgnoreCase(inputRespuesta.getText().toString())) {
-                        labelPalabraSecreta.setText(letra[i].concat(new String(new char[palabraSecreta.length() - i]).replace("\0", "?")));
+
+                if (Utils.isValidLetter(inputRespuesta.getText().toString()) && inputRespuesta.getText().toString() != null) {
+
+                    if (palabraSecreta.contains(inputRespuesta.getText().toString().toLowerCase())) {
+                        isCorrect = true;
+                        labelPalabraSecreta.setText(palabraSecreta.replaceAll("[^" + inputRespuesta.getText().toString().toLowerCase() + "]", "?"));
+                        inputRespuesta.setText("");
+
                     } else {
-                        labelPalabraSecreta.setText(interrogante);
+
+                        isCorrect = false;
+
+                        if (isCorrect == false) {
+
+                            labelPalabraSecreta.setText(palabraSecreta.replaceAll("([A-Za-z])", "?"));
+                            labelContador.setText(getString(R.string.texto_contador) + " " + (++contador) + "\n" + getString(R.string.texto_resultado_incorrecto) + inputRespuesta.getText().toString().toLowerCase());
+                            Toast.makeText(getBaseContext(), "Letra incorrecta", Toast.LENGTH_SHORT).show();
+                            inputRespuesta.setText("");
+                        }
+
+
                     }
                 }
 
+
             }
         });
+
     }
 }
--- app/src/main/java/com/example/rfm/ahorcado2/Utils.java
+++ app/src/main/java/com/example/rfm/ahorcado2/Utils.java
@@ -1,5 +1,19 @@
 package com.example.rfm.ahorcado2;
 
+import java.util.Arrays;
+
 public class Utils {
 
+    /*public static Boolean isCorrecto(String letraInput, String letraOriginal) {
+        return letraInput.equalsIgnoreCase(letraOriginal) ? true : false;
+    }*/
+
+/*    public static String textoResultado(String letraInput, String[] palabraOriginal, String textoResultado) {
+        Arrays.asList(palabraOriginal).stream().map(s -> !s.equalsIgnoreCase("c") ? "?" : s).collect(Collectors.joining())
+        return null;
+    }*/
+
+    public static boolean isValidLetter(String inputText) {
+        return inputText.matches("([A-Za-z])") ? true : false;
+    }
 }
