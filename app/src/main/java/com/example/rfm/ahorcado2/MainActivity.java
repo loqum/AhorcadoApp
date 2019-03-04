a7d780a1acf918a2f55e0ff7ef671817c3d7d23e
loqum <ruben693@gmail.com>
2019-03-04 21:32:33 +0100

    commit

--- app/src/main/java/com/example/rfm/ahorcado2/MainActivity.java
+++ app/src/main/java/com/example/rfm/ahorcado2/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.rfm.ahorcado2;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,7 +18,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView labelContador;
     private EditText inputRespuesta;
     private String palabraSecreta;
+    private Button buttonJugar;
     private int contadorErrores;
+    private String intento;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         labelContador = findViewById(R.id.labelContador);
         labelPalabraSecreta = findViewById(R.id.labelPalabraSecreta);
         inputRespuesta = findViewById(R.id.inputRespuesta);
+        buttonJugar = findViewById(R.id.buttonJugar);
 
         palabraSecreta = "asamblea";
 
@@ -33,27 +40,47 @@ public class MainActivity extends AppCompatActivity {
 
         labelPalabraSecreta.setText(palabraSecreta.replaceAll("([A-Za-z])", "?"));
 
-    }
+        buttonJugar.setOnClickListener(new View.OnClickListener() {
 
-    public void onClickIntentar(View view) {
+            @Override
+            public void onClick(View v) {
 
-        if (Utils.isValidLetter(inputRespuesta.getText().toString())) {
+                onResume();
 
-            if (palabraSecreta.contains(inputRespuesta.getText().toString().toLowerCase())) {
+                if (Utils.isValidLetter(inputRespuesta.getText().toString())) {
 
-                labelPalabraSecreta.setText(palabraSecreta.replaceAll("[^" + inputRespuesta.getText().toString().toLowerCase() + "]", "?"));
-                inputRespuesta.setText("");
+                    inputRespuesta.setText("");
 
-            } else {
+                    intento = sharedPreferences.getString("intento", "Hola");
 
-                labelPalabraSecreta.setText(palabraSecreta.replaceAll("([A-Za-z])", "?"));
-                labelContador.setText(getString(texto_contador, ++contadorErrores));
-                Toast.makeText(getBaseContext(), "Letra incorrecta", Toast.LENGTH_SHORT).show();
-                inputRespuesta.setText("");
+                    if (palabraSecreta.contains(inputRespuesta.getText().toString().toLowerCase())) {
 
+                        labelPalabraSecreta.setText(Utils.isCorrectLetter(palabraSecreta, intento));
+
+
+                    } else {
+
+                        labelPalabraSecreta.setText(Utils.isCorrectLetter(palabraSecreta, intento));
+
+                        labelContador.setText(getString(texto_contador, ++contadorErrores));
+
+                        Toast.makeText(getBaseContext(), "Letra incorrecta", Toast.LENGTH_SHORT).show();
+
+                    }
+                } else {
+                    Toast.makeText(getBaseContext(), "Debes introducir una letra", Toast.LENGTH_LONG);
+                }
             }
-        } else {
-            Toast.makeText(getBaseContext(), "Debes introducir una letra", Toast.LENGTH_LONG);
-        }
+        });
     }
+
+    protected void onResume() {
+        super.onResume();
+
+        sharedPreferences = getSharedPreferences("Intentos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("intento", inputRespuesta.getText().toString());
+        editor.apply();
+    }
+
 }
--- app/src/main/java/com/example/rfm/ahorcado2/Utils.java
+++ app/src/main/java/com/example/rfm/ahorcado2/Utils.java
@@ -5,4 +5,9 @@ class Utils {
     static boolean isValidLetter(String inputText) {
         return inputText.matches("([A-Za-z])");
     }
+
+    static String isCorrectLetter(String palabraSecreta, String inputText) {
+        return palabraSecreta.replaceAll("([^" + inputText.toLowerCase() + "])", "?");
+    }
+
 }
